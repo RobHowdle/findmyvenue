7005efe2a3c61bc574049572aa5b420000f39120
Rob Howdle <robhowdle94@gmail.com>
2024-10-01 21:27:24 +0100

    Updated ASP

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if ($this->app->runningInConsole() || app()->environment('local')) {
+        if ($this->app->runningInConsole() || app()->environment('production')) {
             return;
         }
 
