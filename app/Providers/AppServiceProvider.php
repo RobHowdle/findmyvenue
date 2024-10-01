7d133cbf413936187010335b25b8251452e84ae7
Rob Howdle <robhowdle94@gmail.com>
2024-10-01 21:21:58 +0100

    Adjusted ASP to allow my IP on prod sever

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Contracts\Database\Eloquent\Builder;
+use Symfony\Component\HttpKernel\Exception\ServiceUnavailableHttpException;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +21,21 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if ($this->app->runningInConsole() || app()->environment('local')) {
+            return;
+        }
+
+        $request = request();
+        $allowedIp = '81.99.92.105';
+
+        // Check if the request IP matches your allowed IP
+        if ($request->ip() === $allowedIp) {
+            return; // Don't show maintenance mode for this IP
+        }
+
+        // Check if the application is in maintenance mode
+        if ($this->app->isDownForMaintenance()) {
+            throw new ServiceUnavailableHttpException();
+        }
     }
 }
