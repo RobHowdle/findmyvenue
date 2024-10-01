6d2926f370c98ae8893dfe788f3f29c2c4880334
Rob Howdle <robhowdle94@gmail.com>
2024-10-01 23:18:06 +0100

    Removed Domains

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -33,17 +33,17 @@ class AppServiceProvider extends ServiceProvider
             Log::info('Maintenance mode active for IP: ' . $clientIp);
 
             // Define the main domain to allow access
-            $allowedDomains = ['www.yournextshow.co.uk', 'countdown.yournextshow.co.uk'];
+            // $allowedDomains = ['www.yournextshow.co.uk', 'countdown.yournextshow.co.uk'];
             // Define your allowed IP address
             $allowedIp = '81.99.92.105';
 
             // Check if the request domain matches the allowed domain or the request IP matches
-            if (in_array(Request::getHost(), $allowedDomains) || $clientIp === $allowedIp) {
+            if ($clientIp === $allowedIp) {
                 Log::info('Access granted for IP: ' . $clientIp . ' or domain: ' . Request::getHost());
                 return; // Allow access to the application
             } else {
                 // If not, throw an exception to show the maintenance page
-                Log::info('Access denied for IP - PROBLEM!: ' . $clientIp . Request::getHost());
+                Log::info('Access denied for IP: ' . $clientIp . Request::getHost());
                 throw new ServiceUnavailableHttpException();
             }
         }
