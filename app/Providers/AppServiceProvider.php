9392a19199fa8039ba2cbb02be7fcd1dc4335e4e
Rob Howdle <robhowdle94@gmail.com>
2024-10-02 20:04:31 +0100

    Removed IP Tracking Code form ASP

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,30 +22,30 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // Check if the application is running in console mode
-        if ($this->app->runningInConsole()) {
-            return; // Allow Artisan commands to run
-        }
+        // // Check if the application is running in console mode
+        // if ($this->app->runningInConsole()) {
+        //     return; // Allow Artisan commands to run
+        // }
 
-        // Check if the application is in maintenance mode
-        if ($this->app->isDownForMaintenance()) {
-            $clientIp = Request::ip();
-            Log::info('Maintenance mode active for IP: ' . $clientIp);
+        // // Check if the application is in maintenance mode
+        // if ($this->app->isDownForMaintenance()) {
+        //     $clientIp = Request::ip();
+        //     Log::info('Maintenance mode active for IP: ' . $clientIp);
 
-            // Define the main domain to allow access
-            // $allowedDomains = ['www.yournextshow.co.uk', 'countdown.yournextshow.co.uk'];
-            // Define your allowed IP address
-            $allowedIp = '81.99.92.105';
+        //     // Define the main domain to allow access
+        //     // $allowedDomains = ['www.yournextshow.co.uk', 'countdown.yournextshow.co.uk'];
+        //     // Define your allowed IP address
+        //     $allowedIp = '81.99.92.105';
 
-            // Check if the request domain matches the allowed domain or the request IP matches
-            if ($clientIp === $allowedIp) {
-                Log::info('Access granted for IP: ' . $clientIp . ' or domain: ' . Request::getHost());
-                return; // Allow access to the application
-            } else {
-                // If not, throw an exception to show the maintenance page
-                Log::info('Access denied for IP: ' . $clientIp . Request::getHost());
-                throw new ServiceUnavailableHttpException();
-            }
-        }
+        //     // Check if the request domain matches the allowed domain or the request IP matches
+        //     if ($clientIp === $allowedIp) {
+        //         Log::info('Access granted for IP: ' . $clientIp . ' or domain: ' . Request::getHost());
+        //         return; // Allow access to the application
+        //     } else {
+        //         // If not, throw an exception to show the maintenance page
+        //         Log::info('Access denied for IP: ' . $clientIp . Request::getHost());
+        //         throw new ServiceUnavailableHttpException();
+        //     }
+        // }
     }
 }
