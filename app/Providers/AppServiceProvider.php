0e48903285ad750f8c7485aadf7250f5a1e9ce30
Rob Howdle <robhowdle94@gmail.com>
2024-10-02 20:51:32 +0100

    Removed all code entirely

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,37 +22,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // // Check if the application is running in console mode
-        // if ($this->app->runningInConsole()) {
-        //     return; // Allow Artisan commands to run
-        // }
-
-        // $cloudFlareIp = request()->header('CF-Connecting-IP') ?: request()->ip();
-
-        // // Check if the application is in maintenance mode
-        // if ($this->app->isDownForMaintenance()) {
-        //     $clientIp = Request::ip();
-        //     Log::info('Maintenance mode active for IP: ' . $clientIp);
-        //     Log::info('Clouflare IP: ' . $cloudFlareIp);
-
-        //     // Define your allowed IP address
-        //     $allowedIp = '81.99.92.105';
-
-        //     // Check if request has the Laravel secret bypass cookie for maintenance mode
-        //     if (Request::hasCookie('laravel_maintenance')) {
-        //         Log::info('Access granted via secret key for IP: ' . $clientIp);
-        //         return; // Allow access for secret key
-        //     }
-
-        //     // Check if the request IP matches the allowed IP
-        //     if ($clientIp === $allowedIp) {
-        //         Log::info('Access granted for IP: ' . $clientIp);
-        //         return; // Allow access to the application
-        //     }
-
-        //     // If neither the secret nor the IP is valid, show the maintenance page
-        //     Log::info('Access denied for IP: ' . $clientIp);
-        //     throw new ServiceUnavailableHttpException();
-        // }
+        //
     }
 }
