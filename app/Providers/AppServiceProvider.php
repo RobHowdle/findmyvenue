1fb1715ff6c7626674590caa121d38bb0cca19ee
Rob Howdle <robhowdle94@gmail.com>
2024-10-02 20:24:17 +0100

    Altered ASP

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -32,20 +32,24 @@ class AppServiceProvider extends ServiceProvider
             $clientIp = Request::ip();
             Log::info('Maintenance mode active for IP: ' . $clientIp);
 
-            // Define the main domain to allow access
-            // $allowedDomains = ['www.yournextshow.co.uk', 'countdown.yournextshow.co.uk'];
             // Define your allowed IP address
             $allowedIp = '81.99.92.105';
 
-            // Check if the request domain matches the allowed domain or the request IP matches
-            if ($clientIp === $allowedIp) {
-                Log::info('Access granted for IP: ' . $clientIp . ' or domain: ' . Request::getHost());
-                return; // Allow access to the application
-            } else {
-                // If not, throw an exception to show the maintenance page
-                Log::info('Access denied for IP: ' . $clientIp . Request::getHost());
-                throw new ServiceUnavailableHttpException();
+            // Check if request has the Laravel secret bypass cookie for maintenance mode
+            if (Request::hasCookie('laravel_maintenance')) {
+                Log::info('Access granted via secret key for IP: ' . $clientIp);
+                return; // Allow access for secret key
             }
+
+            // Check if the request IP matches the allowed IP
+            if ($clientIp === $allowedIp) {
+                Log::info('Access granted for IP: ' . $clientIp);
+                return; // Allow access to the application
+            }
+
+            // If neither the secret nor the IP is valid, show the maintenance page
+            Log::info('Access denied for IP: ' . $clientIp);
+            throw new ServiceUnavailableHttpException();
         }
     }
 }
