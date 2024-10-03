d9cedd70e21534eaf39e145c1b75f5d3fa951e02
Rob Howdle <robhowdle94@gmail.com>
2024-10-03 20:55:09 +0100

    Removed Summernote

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,6 @@
 import $ from "jquery";
-import "summernote/dist/summernote-lite.js"; // Non-Bootstrap version
-import "summernote/dist/summernote-lite.css"; // Include the CSS for this version
+// import "summernote/dist/summernote-lite.js"; // Non-Bootstrap version
+// import "summernote/dist/summernote-lite.css"; // Include the CSS for this version
 import Swal from "sweetalert2";
 
 import Alpine from "alpinejs";
