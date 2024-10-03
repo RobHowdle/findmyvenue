e7b397156cc1eaa69665199dfd024e5c2e30af38
Rob Howdle <robhowdle94@gmail.com>
2024-10-03 22:14:15 +0100

    Added conditional to promoter subnav totalProfits

--- resources/views/components/promoter-sub-nav.blade.php
+++ resources/views/components/promoter-sub-nav.blade.php
@@ -1,7 +1,7 @@
 <div class="grid grid-cols-3 place-items-center text-white">
   <div>Events YTD: 30</div>
   <div class="flex">Total Profit YTD:
-    <div id="amountDisplay" class="ml-2">{{ formatCurrency($totalProfits) }}</div>
+    <div id="amountDisplay" class="ml-2">{{ formatCurrency($totalProfits) ?? '£0.00' }}</div>
   </div>
   <div class="rating-wrapper flex flex-row items-center gap-2">Overall Rating:
     <div class="rating flex">
