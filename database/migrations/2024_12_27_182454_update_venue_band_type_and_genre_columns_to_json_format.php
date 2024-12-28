6bd7b8a0de4af990923547b726c83225f9794d46
Rob Howdle <robhowdle94@gmail.com>
2024-12-28 14:38:21 +0000

    Altered venues band type and genre to set values to null

--- database/migrations/2024_12_27_182454_update_venue_band_type_and_genre_columns_to_json_format.php
+++ database/migrations/2024_12_27_182454_update_venue_band_type_and_genre_columns_to_json_format.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -11,9 +12,17 @@ return new class extends Migration
      */
     public function up(): void
     {
+        DB::table('venues')->whereRaw('JSON_VALID(band_type) = 0')->update([
+            'band_type' => DB::raw('NULL'),
+            'genre' => DB::raw('NULL'),
+        ]);
+
         Schema::table('venues', function (Blueprint $table) {
-            $table->json('band_type')->change();
-            $table->json('genre')->change();
+            $table->json('band_type')->nullable(false)->change();
+        });
+
+        Schema::table('venues', function (Blueprint $table) {
+            $table->json('genre')->nullable(false)->change();
         });
     }
 
@@ -23,8 +32,8 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('venues', function (Blueprint $table) {
-            $table->string('band_type')->change();
-            $table->string('genre')->change();
+            $table->text('band_type')->change();
+            $table->text('genre')->change();
         });
     }
 };
