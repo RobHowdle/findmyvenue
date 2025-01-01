86c17987a83fa88844198295ae59bb0a933328dd
Rob Howdle <robhowdle94@gmail.com>
2025-01-01 23:26:17 +0000

    Band Linking bug Fixed

--- resources/views/admin/dashboards/partials/venue-row.blade.php
+++ resources/views/admin/dashboards/partials/venue-row.blade.php
@@ -1,4 +1,4 @@
-<tr class="even:bg-gray-600 odd:dark:bg-gray-800">
+<tr class="odd:bg-gray-800 even:bg-gray-600">
   <td
     class="whitespace-nowrap font-sans text-white sm:px-2 sm:py-3 sm:text-base md:px-6 md:py-2 md:text-lg lg:px-8 lg:py-4">
     {{ $venue->name }}</td>
